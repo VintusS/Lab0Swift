67039612c6ec4fde737722d57a18584136f10a29
VintusS <dragomir.mindrescu@isa.utm.md>
2023-09-11 21:52:15 +0300

    First Class Test

--- Lab0 Swift/main.swift
+++ Lab0 Swift/main.swift
@@ -1,11 +1,25 @@
-//
-//  main.swift
-//  Lab0 Swift
-//
-//  Created by Dragosh on 11.09.2023.
-//
+//Sandbox
 
-import Foundation
+let grade: Character = "A"
+let isNoon: Bool = false
+let arr: [Int] = [9, 2, 19]
+var group: String = "OOP"
 
-print("Hello, World!")
-
+class Car {
+    var speed: Float = 0
+    var isMoving: Bool = false
+    func stop() {
+        isMoving = false
+    }
+    func accelerate(amount: Float){
+        speed += amount
+        isMoving = speed != 0
+    }
+    func printSpeed() {
+        if isMoving {
+            print("The car is moving")
+        } else {
+            print("The car isn't moving")
+        }
+    }
+}
